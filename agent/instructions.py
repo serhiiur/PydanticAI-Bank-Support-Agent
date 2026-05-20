ffdde13d83328ac996f6d47630e88a3fa3095fbf
serhiiur <serhiiur@duck.com>
2026-05-20 13:41:36 +0300

    Add try-except block to check if client is identified

--- agent/instructions.py
+++ agent/instructions.py
@@ -1,9 +1,20 @@
 from pydantic_ai import RunContext
 
 from agent.dependencies import AgentDependencies
+from agent.exceptions import ClientNotFound
 
 
 async def add_client_name(ctx: RunContext[AgentDependencies]) -> str:
-    """Add clients's name to the agent's instructions."""
-    client = await ctx.deps.db.get_client()
+    """Add clients's name to the agent's instructions.
+
+    If the client can't be identified, the corresponding message
+    will be returned.
+
+    :param ctx: Agent's context
+    :return: string containing client's name or error message
+    """
+    try:
+        client = await ctx.deps.db.get_client()
+    except ClientNotFound as e:
+        return str(e)
     return f"The client's name is {client.name}."
